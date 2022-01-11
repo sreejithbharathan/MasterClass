d1212f7a2fa907b6837bfa2f2a49696bddc9b3ee
sreejithbharathan <32619625+sreejithbharathan@users.noreply.github.com>
2022-01-11 08:13:40 -0800

    Add files via upload
    Variable - 1

---
+++ Variables.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Variables {
+
+    public static void main(String[] args){
+        System.out.println("in here");
+
+        int myFirstNumber = (5+10)+(2*10);
+        System.out.println("the value of myFirstNumber is ="+myFirstNumber);
+    }
+}
